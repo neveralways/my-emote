9fcb7fdc8472be5737c9c69376ba4c386fe2f921
Neveralways <pablotenasanchez@gmail.com>
2024-08-01 17:35:08 +0200

    SettingsPanel to select the emotes the user wants.

---
+++ MyEmote/Config.lua
@@ -0,0 +1,77 @@
+local MAX_EMOTES = 8
+local numberOfEmotesChecked = 0
+
+MyEmoteSettings = MyEmoteSettings or {}
+emotesCheckButtons = {}
+
+local optionsPanel = CreateFrame("Frame")
+optionsPanel.name = "My Emote " .. GetAddOnMetadata("MyEmote", "Version")
+
+local scrollFrame = CreateFrame("ScrollFrame", nil, optionsPanel, "UIPanelScrollFrameTemplate")
+scrollFrame:SetPoint("TOPLEFT", 3, -4)
+scrollFrame:SetPoint("BOTTOMRIGHT", -27, 4)
+
+local scrollChild = CreateFrame("Frame")
+scrollFrame:SetScrollChild(scrollChild)
+scrollChild:SetWidth(SettingsPanel:GetWidth()-18)
+scrollChild:SetHeight(1) 
+
+if SettingsPanel then
+    local category, layout = Settings.RegisterCanvasLayoutCategory(optionsPanel, optionsPanel.name)
+    Settings.RegisterAddOnCategory(category)
+end
+
+local title = optionsPanel:CreateFontString("ARTWORK", nil, "GameFontNormalLarge")
+title:SetPoint("TOP")
+title:SetText(optionsPanel.name)
+
+local function createEmoteCheckButton(parent, x, y, text)
+    local cbEmote = CreateFrame("CheckButton", "cbEmote" .. text, parent, "ChatConfigCheckButtonTemplate")
+    cbEmote:SetPoint("TOPLEFT", x, y)
+    getglobal(cbEmote:GetName() .. 'Text'):SetText(text);
+    emotesCheckButtons[text] = cbEmote
+
+    if MyEmoteSettings[text] then
+        cbEmote:SetChecked(true)
+        numberOfEmotesChecked = numberOfEmotesChecked + 1
+    end
+
+    cbEmote:SetScript("OnClick", function(self)
+        if self:GetChecked() then
+            numberOfEmotesChecked = numberOfEmotesChecked + 1
+        else
+            numberOfEmotesChecked = numberOfEmotesChecked - 1
+        end
+
+        if numberOfEmotesChecked > MAX_EMOTES then
+            self:SetChecked(false)
+            numberOfEmotesChecked = numberOfEmotesChecked - 1
+            print("You can't select more than " .. MAX_EMOTES .. " emotes.")
+        else
+            MyEmoteSettings[text] = self:GetChecked() and true or nil
+        end
+    end)
+end
+
+local function OnEvent(self, event, arg1)
+    if event == "ADDON_LOADED" and arg1 == "MyEmote" then
+        local emoteListTitle = scrollChild:CreateFontString("ARTWORK", nil, "GameFontNormalLarge")
+        title:SetPoint("TOPLEFT", 0, 0)
+        title:SetText("Emotes")
+        
+        for i, emote in ipairs(EmoteList) do
+            emoteTextNormalised = emote:sub(1, 1):upper() .. emote:sub(2):lower()
+            createEmoteCheckButton(scrollChild, 50, -20 * i, emoteTextNormalised)
+        end
+        
+        local numberOfEmotes = #EmoteList
+        
+        for i, emote in ipairs(TextEmoteSpeechList) do
+            emoteTextNormalised = emote:sub(1, 1):upper() .. emote:sub(2):lower()
+            createEmoteCheckButton(scrollChild, 50, -20 * (i + numberOfEmotes), emoteTextNormalised)
+        end
+    end
+end
+
+optionsPanel:SetScript("OnEvent", OnEvent)
+optionsPanel:RegisterEvent("ADDON_LOADED")
--- MyEmote/UI.lua
+++ MyEmote/UI.lua
@@ -1,45 +1,74 @@
+local emoteButtons = {}
+
 local wheel = CreateFrame("Frame", "MyEmoteFrame", UIParent, "BackdropTemplate")
 wheel:SetSize(200, 200)
 wheel:Hide()
 
-local emotes = {"Wave", "Cheer", "Dance", "Cry", "Rasp", "Train"}
-local numEmotes = #emotes
-local radius = 80
-local buttonSize = 50
+local function updateEmotes()
+    local selectedEmotes = {}
+    for emote, checkButton in pairs(emotesCheckButtons) do
+        if checkButton:GetChecked() then
+            table.insert(selectedEmotes, emote)
+        end
+    end
 
-for i, emote in ipairs(emotes) do
-    local button = CreateFrame("Button", "EmoteButton" .. i, wheel, "UIPanelButtonTemplate")
-    local transparentTexture = wheel:CreateTexture()
-    transparentTexture:SetColorTexture(0, 0, 0, 0)
-    button:SetHighlightTexture(transparentTexture)
-    button:SetHighlightFontObject(button:GetNormalFontObject())
-    button:SetSize(buttonSize, buttonSize)
-    local angle = (i - 1) * (2 * math.pi / numEmotes)
-    local x = math.cos(angle) * radius
-    local y = math.sin(angle) * radius
-    button:SetPoint("CENTER", wheel, "CENTER", 0, 0)
-    button:SetText(emote)
+    if #selectedEmotes == 0 then
+        selectedEmotes = {"Wave", "Cheer", "Dance", "Cry", "Rasp", "Train", "Flee", "Joke"}
+    end
 
-    local animGroup = button:CreateAnimationGroup()
-    local anim = animGroup:CreateAnimation("Translation")
-    anim:SetOffset(x, y)
-    anim:SetDuration(0.1)
-    anim:SetSmoothing("OUT") 
+    return selectedEmotes
+end
 
-    animGroup:SetScript("OnPlay", function()
-        button:SetScript("OnEnter", nil)
+local function clearEmoteButtons()
+    for i, button in ipairs(emoteButtons) do
+        button:Hide()
+        button:SetParent(nil)
+        _G["EmoteButton" .. i] = nil
+    end
+    emoteButtons = {}
+end
+
+local function createEmoteButtons(wheel, emotes)
+    clearEmoteButtons()
+
+    local numEmotes = #emotes
+    local radius = 100
+    local buttonSize = 50
+
+    for i, emote in ipairs(emotes) do
+        local button = CreateFrame("Button", "EmoteButton" .. i, wheel, "UIPanelButtonTemplate")
+        local transparentTexture = wheel:CreateTexture()
+        transparentTexture:SetColorTexture(0, 0, 0, 0)
+        button:SetHighlightTexture(transparentTexture)
+        button:SetHighlightFontObject(button:GetNormalFontObject())
+        button:SetSize(buttonSize, buttonSize)
+        local angle = (i - 1) * (2 * math.pi / numEmotes)
+        local x = math.cos(angle) * radius
+        local y = math.sin(angle) * radius
         button:SetPoint("CENTER", wheel, "CENTER", 0, 0)
-    end)
-    animGroup:SetScript("OnFinished", function()
-        button:SetPoint("CENTER", wheel, "CENTER", x, y)
-        button:SetScript("OnEnter", function()
-            DoEmote(string.lower(emote))
-            wheel:Hide()
+        button:SetText(emote)
+
+        local animGroup = button:CreateAnimationGroup()
+        local anim = animGroup:CreateAnimation("Translation")
+        anim:SetOffset(x, y)
+        anim:SetDuration(0.1)
+        anim:SetSmoothing("OUT") 
+
+        animGroup:SetScript("OnPlay", function()
+            button:SetScript("OnEnter", nil)
+            button:SetPoint("CENTER", wheel, "CENTER", 0, 0)
         end)
-    end)
-    button.animGroup = animGroup
+        animGroup:SetScript("OnFinished", function()
+            button:SetPoint("CENTER", wheel, "CENTER", x, y)
+            button:SetScript("OnEnter", function()
+                DoEmote(string.lower(emote))
+                wheel:Hide()
+            end)
+        end)
+        button.animGroup = animGroup
 
-
+        table.insert(emoteButtons, button)
+    end
 end
 
 local function ToggleWheel()
@@ -49,7 +78,10 @@ local function ToggleWheel()
     if wheel:IsShown() then
         wheel:Hide()
     else
-        for i = 1, numEmotes do
+        local selectedEmotes = updateEmotes()
+        createEmoteButtons(wheel, selectedEmotes)
+        
+        for i = 1, #selectedEmotes do
             local button = _G["EmoteButton" .. i]
             button.animGroup:Stop()
             button.animGroup:Play()
